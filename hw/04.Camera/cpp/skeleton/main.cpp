49757e07c9b9db9bb4aed0ca8e5f3d38d745ba21
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-05-15 19:29:47 +0900

    rotation 둔감하게 설정

--- hw/04.Camera/cpp/skeleton/main.cpp
+++ hw/04.Camera/cpp/skeleton/main.cpp
@@ -184,8 +184,15 @@ void compose_imgui_frame()
     if(ImGui::SliderFloat3("Tranlsate", glm::value_ptr(vec_cam_pos), -10.0f, 10.0f)){
       g_camera.set_position(vec_cam_pos);
     }
-    if(ImGui::gizmo3D("Rotation", quat_cam)){
-      g_camera.set_rotation(quat_cam);
+    if (ImGui::gizmo3D("Rotation", quat_cam)) {
+      // 델타 회전 계산
+      glm::quat delta_quat = quat_cam * glm::inverse(prev_quat_cam);
+      // 델타 회전 스케일링 (회전 민감도 감소)
+      float rotation_scale = 0.5f; // 민감도 조절 스케일 (값이 작을수록 더 둔감)
+      delta_quat = glm::slerp(glm::quat(), delta_quat, rotation_scale);
+      // 최종 회전 적용
+      glm::quat final_quat = delta_quat * prev_quat_cam;
+      g_camera.set_rotation(final_quat);
     }
 
     ImGui::End();
