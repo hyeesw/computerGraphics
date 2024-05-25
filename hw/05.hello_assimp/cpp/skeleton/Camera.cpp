47cf832015eee498f8cf56ca2582ac41ca0be533
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-05-25 10:39:58 +0900

    update Camera.cpp

--- hw/05.hello_assimp/cpp/skeleton/Camera.cpp
+++ hw/05.hello_assimp/cpp/skeleton/Camera.cpp
@@ -1,113 +1,95 @@
 #include "Camera.h"
 
-// TODO: fill up the following function properly 
-void Camera::set_rotation(const glm::quat& _q)
-{
+// FIXED
+void Camera::set_rotation(const glm::quat& _q) {
+    // input 쿼터니언을 이용해 회전 행렬을 만든다
+    glm::mat4 rotation_matrix = glm::mat4_cast(_q);
 
+    // 회전 행렬을 이용해 front, up, right 방향을 계산한다
+    front_dir_ = glm::normalize(glm::vec3(rotation_matrix * glm::vec4(0.0f, 0.0f, -1.0f, 0.0f)));
+    up_dir_ = glm::normalize(glm::vec3(rotation_matrix * glm::vec4(0.0f, 1.0f, 0.0f, 0.0f)));
+    right_dir_ = glm::normalize(glm::vec3(rotation_matrix * glm::vec4(1.0f, 0.0f, 0.0f, 0.0f)));
 }
 
-// TODO: re-write the following function properly 
-const glm::quat Camera::get_rotation() const
-{
-  return glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+// FIXED
+const glm::quat Camera::get_rotation() const {
+    // 방향 벡터를 이용해 회전행렬을 만든다
+    glm::mat4 rotation_matrix(
+        glm::vec4(right_dir_, 0.0f),
+        glm::vec4(up_dir_, 0.0f),
+        glm::vec4(-front_dir_, 0.0f), // front_dir은 -z 방향
+        glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)
+    );
+
+    return glm::quat_cast(rotation_matrix); //회전행렬을 쿼터니언으로 변환
 }
 
-// TODO: fill up the following function properly 
-void Camera::set_pose(const glm::quat& _q, const glm::vec3& _t)
-{
-
+// FIXED
+void Camera::set_pose(const glm::quat& _q, const glm::vec3& _t) {
+    set_rotation(_q);
+    position_ = _t;
 }
 
-// TODO: fill up the following function properly 
-void Camera::get_pose(glm::quat& _q, glm::vec3& _t) const
-{
-
+// FIXED
+void Camera::get_pose(glm::quat& _q, glm::vec3& _t) const {
+    _q = get_rotation();
+    _t = position_;
 }
 
-// TODO: rewrite the following function properly 
-const glm::mat4 Camera::get_pose() const
-{
-  return  glm::mat4(1.0f);
+// FIXED
+const glm::mat4 Camera::get_pose() const {
+  return glm::translate(glm::mat4(1.0f), position_) * glm::mat4_cast(get_rotation());
 }
 
-// TODO: fill up the following function properly 
-void Camera::set_pose(const glm::mat4& _frame)
-{
-  // +x_cam: right direction of the camera    (it should be a unit vector whose length is 1)
-  // right_dir_ = ..... ; // +x
-  // +y_cam: up direction of the camera       (it should be a unit vector whose length is 1)   
-  // up_dir_    = ..... ;    // +y
-  // -z_cam: front direction of the camera    (it should be a unit vector whose length is 1)
-  // front_dir_ = ..... ;    // -z
-  // pos_cam: position of the camera
-  // position_  = ..... ;    // pos
+// FIXED
+void Camera::set_pose(const glm::mat4& _frame) {
+  position_ = glm::vec3(_frame[3]);
+  set_rotation(glm::quat_cast(_frame));
 }
 
-// TODO: fill up the following function properly 
-void Camera::set_pose(const glm::vec3& _pos, const glm::vec3& _at, const glm::vec3& _up_dir)
-{
-  // up_dir_    = ..... ;
-  // front_dir_ = ..... ;    // -z_cam direction
-  // right_dir_ = ..... ;    // +x_cam direction
-  // up_dir_    = ..... ;    // +y_cam direction
-
-  // position_  = ..... ;      // pos
+// FIXED
+void Camera::set_pose(const glm::vec3& _pos, const glm::vec3& _at, const glm::vec3& _up_dir){
+  position_ = _pos; 
+  front_dir_ = glm::normalize(_at - _pos);
+  up_dir_    = glm::normalize(_up_dir);
+  right_dir_ = glm::normalize(glm::cross(front_dir_, up_dir_));
+  up_dir_ = glm::cross(right_dir_, front_dir_); // Re-calculate to ensure orthogonality
 }
 
-// TODO: rewrite the following function properly 
+// FIXED
 const glm::mat4 Camera::get_view_matrix() const
 {
-  return glm::mat4(1.0f);
+  return glm::lookAt(position_, position_ + front_dir_, up_dir_);
 }
 
-// TODO: rewrite the following function properly 
+// FIXED
 const glm::mat4 Camera::get_projection_matrix() const
 {
-  // TODO: Considering the followings,
-  //       You must return a proper projection matrix
-  //       i) camera mode: it can be either kOrtho or kPerspective
-  //       ii) zoom-in/-out: if the camera mode is kOrtho, 
-  //                         utilize ortho_scale_ for zoom-in/-out
-  //                         if the camera mode is kPerspective,
-  //                         utilize fovy_ for zoom-in/-out
-  //       iii) aspect ratio: utilize aspect_ in the both camera modes
-  //       iv) near/far clipping planes: utilize near_, far_
-
-  return glm::mat4(1.0f);
+if (mode_ == kPerspective) {
+        return glm::perspective(glm::radians(fovy_), aspect_, near_, far_);
+    } else { // kOrtho
+        float ortho_half_height = ortho_scale_ * aspect_;
+        float ortho_half_width = ortho_scale_;
+        return glm::ortho(-ortho_half_width, ortho_half_width, -ortho_half_height, ortho_half_height, near_, far_);
+    }
 }
 
-// TODO: fill up the following functions properly 
-void Camera::move_forward(float delta)
-{
-
+// FIXED
+void Camera::move_forward(float delta) {
+  position_ += front_dir_ * delta;
 }
-
-// TODO: fill up the following functions properly 
-void Camera::move_backward(float delta)
-{
-
+void Camera::move_backward(float delta){
+  position_ -= front_dir_ * delta;
 }
-
-// TODO: fill up the following functions properly 
-void Camera::move_left(float delta)
-{
-
+void Camera::move_left(float delta) {
+  position_ -= right_dir_ * delta;
 }
-
-// TODO: fill up the following functions properly 
-void Camera::move_right(float delta)
-{
-
+void Camera::move_right(float delta){
+  position_ += right_dir_ * delta;
 }
-
-// TODO: fill up the following functions properly 
-void Camera::move_up(float delta)
-{
-
+void Camera::move_up(float delta){
+  position_ += up_dir_ * delta;
 }
-
-// TODO: fill up the following functions properly 
-void Camera::move_down(float delta)
-{
-
+void Camera::move_down(float delta){
+  position_ -= up_dir_ * delta;
 }
