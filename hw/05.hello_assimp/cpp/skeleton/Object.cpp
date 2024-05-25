d8f93c601bc59e20d636f9f48d4a1f37c6c31dd5
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-05-25 10:59:23 +0900

    Object.cpp 물체 화면에 보이기

--- hw/05.hello_assimp/cpp/skeleton/Object.cpp
+++ hw/05.hello_assimp/cpp/skeleton/Object.cpp
@@ -2,37 +2,69 @@
 
 void Object::init_buffer_objects() 
 {
-    // TODO
+    // Create buffers
+    glGenBuffers(1, &position_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    glBufferData(GL_ARRAY_BUFFER, pmesh_->mNumVertices * 3 * sizeof(float), pmesh_->mVertices, GL_STATIC_DRAW);
+
+    if (pmesh_->HasVertexColors(0))
+    {
+        is_color = true;
+        glGenBuffers(1, &color_buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+        glBufferData(GL_ARRAY_BUFFER, pmesh_->mNumVertices * 3 * sizeof(float), pmesh_->mColors[0], GL_STATIC_DRAW);
+    }
+
+    for (unsigned int i = 0; i < pmesh_->mNumFaces; ++i)
+    {
+        Face face;
+        const aiFace& ai_face = pmesh_->mFaces[i];
+
+        glGenBuffers(1, &face.index_buffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, face.index_buffer);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, ai_face.mNumIndices * sizeof(unsigned int), ai_face.mIndices, GL_STATIC_DRAW);
+
+        face.num_indices = ai_face.mNumIndices;
+        faces.push_back(face);
+    }
 }
 
 glm::mat4 Object::get_model_matrix() const
 {
     glm::mat4 mat_model = glm::mat4(1.0f);
-    
-    // TODO
+
+    mat_model = glm::translate(mat_model, vec_translate_);
+    mat_model *= glm::mat4_cast(quat_rotate_);
+    mat_model = glm::scale(mat_model, vec_scale_);
 
     return mat_model;
 }
 
 void Object::draw(int loc_a_position, int loc_a_color)
 {
-    // TODO
-}
-    
-void Object::print_info()
-{
-    std::cout << "print mesh info" << std::endl;
+    // Bind position buffer
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    glVertexAttribPointer(loc_a_position, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(loc_a_position);
 
-    std::cout << "num vertices " << pmesh_->mNumVertices << std::endl;
-    for (unsigned int i = 0; i < pmesh_->mNumVertices; ++i)
+    // Bind color buffer if available
+    if (is_color)
     {
-        aiVector3D vertex = pmesh_->mVertices[i];
-        std::cout << "  vertex  (" << vertex.x << ", " << vertex.y << ", " << vertex.z << ")" << std::endl;
+        glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+        glVertexAttribPointer(loc_a_color, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(loc_a_color);
+    }
 
-        if (pmesh_->mColors[0] != NULL)
-        {
-            aiColor4D color = pmesh_->mColors[0][i];
-            std::cout << "  color  (" << color.r << ", " << color.g << ", " << color.b << ", " << color.a << ")" << std::endl;
-        }
+    // Draw all faces
+    for (const Face& face : faces)
+    {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, face.index_buffer);
+        glDrawElements(GL_TRIANGLES, face.num_indices, GL_UNSIGNED_INT, 0);
+    }
+
+    glDisableVertexAttribArray(loc_a_position);
+    if (is_color)
+    {
+        glDisableVertexAttribArray(loc_a_color);
     }
 }
